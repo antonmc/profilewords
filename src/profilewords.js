5e5abe686d4fe5c2860e56b34a4fdf3fae8a4c2c
Anton McConville <antonm@Antons-Work-MacBook-Pro.local>
2014-06-17 23:18:57 -0400

    Adding refresh capability
    Now we can redraw a cloud without retrieving the data for it again.
    Also beginning some refactoring work.

--- src/profilewords.js
+++ src/profilewords.js
@@ -1,69 +1,90 @@
+/* ProfileWords (c) Anton McConville 2014 */
+
+/* Definitions and globals */
+
+var WARNING_REFRESH = 'Not so fast - nothing to refresh yet - submit a query first!';
+var WARNING_RATE_LIMIT = 'You have reached your rate limit for this 15 minute window';
+var WARNING_INVALID_ID = 'There was no data for that twitter id. Is it an actual id? Please try another';
+
+var latestProfileData;
+var latestRateData = 15;
+
 var getParameter = function() {
-        var vars = {};
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
-        function(m,key,value) {
-          vars[key] = value;
-        });
-        return vars;
-    }
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+}
     
-    var renderWordle = function () {
+function renderWordle(){
         
-        var idField = document.getElementById( 'twitterIDfield' );
-        
-        console.log( 'render wordle: ' + idField.value );
-        
-        if( idField.value ){
-        
-            var path = '/words/' + idField.value ;
-            
-            var oauth_token = getParameter()['oauth_token'];
-            
-            var parameters;
-            
-            if( oauth_token ){                
-                console.log( 'read oauth token: ' + oauth_token );
-                parameters = 'oauth_token=' + oauth_token;
-                
-            }
-            
-            var xhr = new XMLHttpRequest();
-               
-                xhr.open("GET", path, true);
-                xhr.onload = function (e) {
-                  if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        var res = JSON.parse( xhr.responseText );
-                        
-                        if( res.outcome === 'success' ){
-                            parseProfileData( res.profiles );
-                            drawScrollbar( res.budget );
-                        }else{
-                            showWarning();
-                            drawScrollbar( res.budget );
-                        }
-                        
-                    } else {
-                      console.error(xhr.statusText);
-                    }
-                  }
-                };
-            
-                xhr.setRequestHeader( "oauth_token", oauth_token );
-            
-                xhr.onerror = function (e) {
-                    console.error(xhr.statusText);
-                    showWarning();
-                };
-                xhr.send( parameters );
+    var idField = document.getElementById( 'twitterIDfield' );
+
+    console.log( 'render wordle: ' + idField.value );
+
+    if( idField.value ){
+
+        var path = '/words/' + idField.value ;
+
+        var oauth_token = getParameter()['oauth_token'];
+
+        var parameters;
+
+        if( oauth_token ){                
+            console.log( 'read oauth token: ' + oauth_token );
+            parameters = 'oauth_token=' + oauth_token;
+
         }
-    };
+
+        var xhr = new XMLHttpRequest();
+
+            xhr.open("GET", path, true);
+            xhr.onload = function (e) {
+              if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var res = JSON.parse( xhr.responseText );
+
+                    if( res.outcome === 'success' ){
+
+                        latestProfileData = res.profiles;
+
+                        parseProfileData( latestProfileData );
+                        drawScrollbar( res.budget );
+                    }else{
+                        showWarning( WARNING_INVALID_ID );
+                        drawScrollbar( res.budget );
+                    }
+
+                } else {
+                  console.error(xhr.statusText);
+                }
+              }
+            };
+
+            xhr.setRequestHeader( "oauth_token", oauth_token );
+
+            xhr.onerror = function (e) {
+                console.error(xhr.statusText);
+                showWarning( WARNING_INVALID_ID );
+            };
+            xhr.send( parameters );
+    }
+}
 
 
+function refresh(){
+    if( latestProfileData ){
+        parseProfileData( latestProfileData );   
+    }else{
+        showWarning( WARNING_REFRESH );
+    }
+}
 
 
 document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
+ctx = canvas.getContext('2d');
                              
 function drawScrollbar( remaining ) {
     
@@ -94,10 +115,10 @@ function drawScrollbar( remaining ) {
 drawScrollbar(15);
 
 
-function showWarning(){
+function showWarning( warning ){
     var warningText =   '<div class="alert alert-warning alert-dismissable">' +
                         '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
-                        '<strong>Warning! </strong> There was no data for that twitter id. Is it an actual id? Please try another.</div>';  
+                        '<strong>Warning! </strong>' + warning + '</div>';  
     
     var warningArea = document.getElementById( 'warningArea' );
     
@@ -145,36 +166,40 @@ function parseProfileData( data ) {
 }
 
 function generate() {
-  layout.font('Varela').spiral('archimedean');
-//    layout.font('love-ya-like-a-sister, fantasy').spiral('archimedean');
-//    layout.font('amatic-sc, sans-serif').spiral('archimedean');
+    layout.font('Varela').spiral('archimedean');
+    //    layout.font('love-ya-like-a-sister, fantasy').spiral('archimedean');
+    //    layout.font('amatic-sc, sans-serif').spiral('archimedean');
 
-  fontSize = d3.scale['linear']().range([5, 75]);
-  if (tags.length) fontSize.domain([+tags[tags.length - 1].value || 1, +tags[0].value]);
-  complete = 0;
-  statusText.style("display", null);
-  words = [];
-  layout.stop().words(tags.slice(0, max = 70 )).start();
+    fontSize = d3.scale['linear']().range([5, 75]);
+    if (tags.length) fontSize.domain([+tags[tags.length - 1].value || 1, +tags[0].value]);
+    complete = 0;
+    statusText.style("display", null);
+    words = [];
+    layout.stop().words(tags.slice(0, max = 70 )).start();
 }
 
 function progress(d) {
-  statusText.text(++complete + "/" + max);
+    statusText.text(++complete + "/" + max);
 }
 
+function randomColor( scheme ){
+    return scheme[ Math.floor( ( Math.random() * scheme.length ) ) ]
+}
 
 function customFill(d){    
     
-    var Maddening_Caravan = [ 'FAD089', 'FF9C5B', 'F5634A', 'ED303C', '3B8183' ];    
+    var Maddening_Caravan = [ '#FAD089', '#FF9C5B', '#F5634A', '#ED303C', '#3B8183' ];    
     var Kuler = [ 'FF530D', 'E82C0C', 'FF0000', 'E80C7A', 'FF0DFF' ];    
-    var High_Rollers = [ 'FF534E', 'FFD7AC', 'BED194', '499989', '176785' ];
-    var Primavera = [ '5EBFAD', 'F1F2D8', 'F2A03D', 'F2541B', 'D94423' ];    
-    var Winter_Snowfall = [ '273F5A', 'C6DBF3', '4B81A5', '74A0BF', '98C4DA' ];    
-    var Earth = [ 'cbc4ba', 'd4806d', 'c2be98', 'e3cb92', '695d4f' ];
-    var Anton = [ 'C3DBCE', 'FF3300', '170746', 'D7E7E7' ];
-    var Adjustment = [ '5C8C7E', '47738c', '8A9FB0', '95AA99', '2f4d57' ];
-    
+    var High_Rollers = [ '#FF534E', '#FFD7AC', '#BED194', '#499989', '#176785' ];
+    var Primavera = [ '#5EBFAD', '#F1F2D8', '#F2A03D', '#F2541B', '#D94423' ];    
+    var Winter_Snowfall = [ '#273F5A', '#C6DBF3', '#4B81A5', '#74A0BF', '#98C4DA' ];    
+    var Earth = [ '#cbc4ba', '#d4806d', '#c2be98', '#e3cb92', '#695d4f' ];
+    var Anton = [ '#C3DBCE', '#FF3300', '#170746', '#D7E7E7' ];
+    var Adjustment = [ '#5C8C7E', '#47738c', '#8A9FB0', '#95AA99', '#2f4d57' ];
+    var Lifeguard = [ '#b6c6c6 ', '#ec6c52', '#f2b26f', '#ABC5CB', '#93ADB6', '#7cb2c4' ];
+    var Drop = [ 'ed7157' ]
         
-    fillColor =  '#' + Earth[ Math.floor( ( Math.random()* Earth.length ) ) ];
+    fillColor =  randomColor( Earth );
     
     return fillColor;
 }
@@ -303,16 +328,16 @@ d3.select("#random-palette").on("click", function() {
       scale = d3.scale.linear(),
       arc = d3.svg.arc().innerRadius(0).outerRadius(r);
 
-  getAngles();
+getAngles();
 
-  function getAngles() {
+function getAngles() {
     count = 2;
     from = 0;
     to = 90;
     update();
-  }
+}
 
-  function update() {
+function update() {
     scale.domain([0, count - 1]).range([from, to]);
     var step = (to - from) / count;
 
